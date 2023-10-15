bfa3c05b1222534b9f4d67582d0b86c1b405bc7c
swetarajak <swetarajak001@gmail.com>
2023-10-15 10:04:21 +0530

    Please merge my request

---
+++ Java/sweta.java
@@ -0,0 +1,5 @@
+public class sweta{
+    public static void main(String[] args){
+            System.out.println("My name is Sweta Rajak, I am Contributing in Hacktober Open Source Project");
+    }
+}
